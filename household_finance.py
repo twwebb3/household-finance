82e566ffc0895bb58fcc4a8223ff14d73ba6da6e
twwebb3 <twwebb1@me.com>
2021-10-14 00:39:05 -0400

    household_finanance.py: add fields to forms, make defaults in forms dynamic to what's in the DB.

--- household_finance.py
+++ household_finance.py
@@ -73,6 +73,12 @@ class NameForm(FlaskForm):
 
 class BudgetRemainingForm(FlaskForm):
     expenditure_type = SelectField('Expenditure Type:', choices=['Mallory Personal Budget', 'TW Personal Budget'])
+    year = SelectField('Year:',
+                       choices=[year for year in range(datetime.now().year-1,datetime.now().year+2)],
+                       default=datetime.now().year)
+    month = SelectField('Month:',
+                        choices=[month for month in range(1, 13)],
+                        default=datetime.now().month)
     submit = SubmitField('Submit')
 
 class DefaultsEntryForm(FlaskForm):
@@ -114,6 +120,15 @@ def internal_server_error(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = BudgetRemainingForm()
+
+    exp1 = ExpenditureType.query.with_entities(ExpenditureType.expenditure_type)
+    exp_type = []
+    for j in exp1:
+        temp = j._asdict()
+        exp_type.append(temp['expenditure_type'])
+
+    form.expenditure_type.choices = exp_type
+
     amount=''
     exp_hist_df = pd.DataFrame({})
     if form.validate_on_submit():
